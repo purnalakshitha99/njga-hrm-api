6d65d547cb48374ca140ec3613a291997abbd484
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-02-20 14:51:01 +0530

    initial commit

---
+++ src/main/java/lk/zerocode/api/NjgaHrmApiApplication.java
@@ -0,0 +1,13 @@
+package lk.zerocode.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjgaHrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjgaHrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/zerocode/api/NjgaHrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.zerocode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjgaHrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
