5db59b075f531d73bf086fd4ce0bac41379b2a40
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-02-21 13:58:25 +0530

    added dep id to the table

--- src/main/java/lk/zerocode/api/model/Department.java
+++ src/main/java/lk/zerocode/api/model/Department.java
@@ -13,6 +13,7 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String depId;
     private String name;
 
     @ManyToMany(mappedBy = "departmentList")
