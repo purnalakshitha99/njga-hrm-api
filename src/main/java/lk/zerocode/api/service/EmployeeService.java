eb8b45c24697635aa2d4c71a1bfb8512a61c1514
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2024-02-23 21:41:23 +0530

    dependent service updated

--- src/main/java/lk/zerocode/api/service/EmployeeService.java
+++ src/main/java/lk/zerocode/api/service/EmployeeService.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-    BasicDetailsResponse saveBasicDetails(Employee employee) throws EmployeeNotFoundException;}
+    BasicDetailsResponse saveBasicDetails(Employee employee) throws EmployeeNotFoundException;
+}
