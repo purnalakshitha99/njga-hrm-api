2a620461399dc87e64f226b4232421dec48a15a3
SasidaDilhan <sasindadilhan98@gmail.com>
2024-02-24 17:07:02 +0530

    Getting viduths pull

--- src/main/java/lk/zerocode/api/repository/EmployeeRepository.java
+++ src/main/java/lk/zerocode/api/repository/EmployeeRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
+    Optional<Employee> findEmployeeByEmpId(String emp_id);
+
 }
--- src/main/java/lk/zerocode/api/service/impl/BasicDetailsImpl.java
+++ src/main/java/lk/zerocode/api/service/impl/BasicDetailsImpl.java
@@ -22,23 +22,30 @@ public class BasicDetailsImpl implements EmployeeService {
     @Override
     public BasicDetailsResponse saveBasicDetails(EmployeeRequest employeeRequest) throws EmployeeNotFoundException {
         Employee employee = new Employee();
-        employee.setEmpId(employeeRequest.getEmp_id());
-        employee.setFirstName(employeeRequest.getFirst_name());
-        employee.setLastName(employeeRequest.getLast_name());
-        employee.setDob(employeeRequest.getDob());
-        employee.setAddress(employeeRequest.getAddress());
-        employee.setContactNumber(employeeRequest.getContact_number());
-        employee.setEmpId(employeeRequest.getEmail());
-        employee.setImagePath(employeeRequest.getImage_path());
-        employee.setNic(employeeRequest.getNic());
-        employee.setWorkTelephone(employeeRequest.getWork_telephone());
-        employee.setGender(employeeRequest.getGender());
+        Optional<Employee> empOpt = employeeRepository.findEmployeeByEmpId(employeeRequest.getEmp_id());
+        if (!empOpt.isPresent()){
+            employee.setEmpId(employeeRequest.getEmp_id());
+            employee.setFirstName(employeeRequest.getFirst_name());
+            employee.setLastName(employeeRequest.getLast_name());
+            employee.setDob(employeeRequest.getDob());
+            employee.setAddress(employeeRequest.getAddress());
+            employee.setContactNumber(employeeRequest.getContact_number());
+            employee.setEmpId(employeeRequest.getEmail());
+            employee.setImagePath(employeeRequest.getImage_path());
+            employee.setNic(employeeRequest.getNic());
+            employee.setWorkTelephone(employeeRequest.getWork_telephone());
+            employee.setGender(employeeRequest.getGender());
 
-        employeeRepository.save(employee);
+            employeeRepository.save(employee);
 
-        BasicDetailsResponse basicDetailsResponse = BasicDetailsResponse.builder()
-                .id(employee.getId())
-                .build();
-     return basicDetailsResponse;
+            BasicDetailsResponse basicDetailsResponse = BasicDetailsResponse.builder()
+                    .id(employee.getId())
+                    .build();
+            return basicDetailsResponse;
+        }
+        else {
+            throw new EmployeeNotFoundException("Employee id Exist");
+        }
+
     }
 }
