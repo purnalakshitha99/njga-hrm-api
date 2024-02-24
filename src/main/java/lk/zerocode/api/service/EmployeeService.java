e6be38e33ebf5682f132ef8bfd4ed7723564cdd0
SasidaDilhan <sasindadilhan98@gmail.com>
2024-02-24 16:37:20 +0530

    Employee basic details created

--- src/main/java/lk/zerocode/api/service/EmployeeService.java
+++ src/main/java/lk/zerocode/api/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package lk.zerocode.api.service;
 
+import lk.zerocode.api.controller.request.EmployeeRequest;
 import lk.zerocode.api.controller.response.BasicDetailsResponse;
 import lk.zerocode.api.exceptions.EmployeeNotFoundException;
 import lk.zerocode.api.model.Employee;
@@ -7,5 +8,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-    BasicDetailsResponse saveBasicDetails(Employee employee) throws EmployeeNotFoundException;
+    BasicDetailsResponse saveBasicDetails(EmployeeRequest employeeRequest) throws EmployeeNotFoundException;
 }
