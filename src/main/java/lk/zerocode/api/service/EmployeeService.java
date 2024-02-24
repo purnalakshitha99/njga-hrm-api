dce515cdd8d7954cc341662e13142387605022b5
SasidaDilhan <sasindadilhan98@gmail.com>
2024-02-24 20:22:36 +0530

    Get by emp id and email done

--- src/main/java/lk/zerocode/api/service/EmployeeService.java
+++ src/main/java/lk/zerocode/api/service/EmployeeService.java
@@ -1,12 +1,14 @@
 package lk.zerocode.api.service;
 
-import lk.zerocode.api.controller.request.EmployeeRequest;
+import lk.zerocode.api.controller.request.BasicDetailsRequest;
 import lk.zerocode.api.controller.response.BasicDetailsResponse;
+import lk.zerocode.api.controller.response.IdResponse;
 import lk.zerocode.api.exceptions.EmployeeNotFoundException;
-import lk.zerocode.api.model.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-    BasicDetailsResponse saveBasicDetails(EmployeeRequest employeeRequest) throws EmployeeNotFoundException;
+    IdResponse saveBasicDetails(BasicDetailsRequest basicDetailsRequest) throws EmployeeNotFoundException;
+    BasicDetailsResponse getByEmpId(String id) throws EmployeeNotFoundException;
+    BasicDetailsResponse getByEmpEmail(String email) throws EmployeeNotFoundException;
 }
