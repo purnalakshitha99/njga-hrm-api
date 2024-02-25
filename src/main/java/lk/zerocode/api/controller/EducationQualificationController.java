032141470c2a5f2cb66af10d377204589480ff01
Mohamed-Inthisham <black.devil9b@gmail.com>
2024-02-25 23:00:27 +0530

    education qualification delete part done

--- src/main/java/lk/zerocode/api/controller/EducationQualificationController.java
+++ src/main/java/lk/zerocode/api/controller/EducationQualificationController.java
@@ -4,10 +4,7 @@ import lk.zerocode.api.controller.request.EducationQualificationRequest;
 import lk.zerocode.api.controller.response.EducationQualificationResponse;
 import lk.zerocode.api.service.EducationQualificationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -15,8 +12,12 @@ public class EducationQualificationController {
 
     private EducationQualificationService educationQualificationService;
 
-    @PostMapping(value = "/employees/{employee-id}",headers ="X-API-VERSION=V1")
+    @PostMapping(value = "/employees/{employee-id}/qualifications",headers ="X-API-VERSION=V1")
     public EducationQualificationResponse createQualification(@PathVariable("employee-id") Long id, @RequestBody EducationQualificationRequest educationQualificationRequest){
-    return educationQualificationService.create(id,educationQualificationRequest);
+        return educationQualificationService.create(id,educationQualificationRequest);
+    }
+    @DeleteMapping(value = "/qualifications/{qualification-id}/{employee-id}",headers ="X-API-VERSION=V1")
+    public void deleteQualification(@PathVariable("qualification-id")Long id,@PathVariable("employee-id")Long employeeId ){
+        educationQualificationService.delete(id,employeeId);
     }
 }
