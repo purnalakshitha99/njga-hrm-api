012a0f3710d024deb4657e9990cfe2dd20fad241
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2024-02-26 14:52:31 +0530

    department error fixed

--- src/main/java/lk/zerocode/api/controller/DependentController.java
+++ src/main/java/lk/zerocode/api/controller/DependentController.java
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-//package lk.zerocode.api.controller;
-//
-//import lk.zerocode.api.controller.request.DependentDetailReq;
-//import lk.zerocode.api.service.EmployeeService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@AllArgsConstructor
-//public class DependentController {
-//    private EmployeeService employeeService;
-//    @PostMapping(value = "/students",headers = "version=v1")
-//    public void addDependent(@RequestBody DependentDetailReq dependentDetailReq, @PathVariable Long id){
-//        employeeService.addDependent(dependentDetailReq,id);
-//    }
-//}
-=======
 package lk.zerocode.api.controller;
 
 import lk.zerocode.api.controller.request.DependentDetailRequest;
@@ -50,5 +29,3 @@ public class DependentController {
         return dependentService.updateDependentDetail(id,dependentDetailRequest);
     }
 }
-
->>>>>>> main
