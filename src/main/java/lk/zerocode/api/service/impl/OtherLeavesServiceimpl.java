e4b549113b3048e8dedd628afada56bcf8211fc6
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-02-28 09:45:28 +0000

    add otherleaves files

---
+++ src/main/java/lk/zerocode/api/service/impl/OtherLeavesServiceimpl.java
@@ -0,0 +1,10 @@
+package lk.zerocode.api.service.impl;
+import lk.zerocode.api.service.OtherLeavesService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+@Service
+@AllArgsConstructor
+public class OtherLeavesServiceimpl implements OtherLeavesService {
+
+
+}
