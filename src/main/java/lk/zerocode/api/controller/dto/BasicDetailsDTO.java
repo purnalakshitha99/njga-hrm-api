b1fd146f0548c73ba6ef48642455bef8413ead8c
SasidaDilhan <sasindadilhan98@gmail.com>
2024-03-03 18:57:20 +0530

    trying to convert DTO

---
+++ src/main/java/lk/zerocode/api/controller/dto/BasicDetailsDTO.java
@@ -0,0 +1,27 @@
+package lk.zerocode.api.controller.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lk.zerocode.api.model.Gender;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+public class BasicDetailsDTO {
+
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private LocalDate dob;
+    private String address;
+    private String contact_number;
+    private String email;
+    private String image_path;
+    private String nic;
+    private String work_telephone;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
