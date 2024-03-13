c74ffa80b658a70d4ab14b7c605503111892fed8
DehaniPathirana <dehanikavindya3@gmail.com>
2024-03-13 11:23:23 +0530

    update code using modlemappers

--- src/main/java/lk/zerocode/api/controller/PreviousWorkHistoryController.java
+++ src/main/java/lk/zerocode/api/controller/PreviousWorkHistoryController.java
@@ -1,10 +1,12 @@
 package lk.zerocode.api.controller;
 
 
+import lk.zerocode.api.controller.dto.PreviousWorkHistoryDTO;
 import lk.zerocode.api.controller.request.PreviousWorkHistoryRequest;
 
 import lk.zerocode.api.controller.response.PreviousWorkHistoryIdResponse;
 import lk.zerocode.api.controller.response.PreviousWorkHistoryResponse;
+import lk.zerocode.api.controller.response.PreviousWorkHistoryResponse01;
 import lk.zerocode.api.exceptions.EmployeeNotFoundException;
 import lk.zerocode.api.exceptions.PreviousWorkHistoryNotFoundException;
 import lk.zerocode.api.service.PreviousWorkHistoryService;
@@ -19,20 +21,20 @@ public class PreviousWorkHistoryController {
     private PreviousWorkHistoryService previousWorkHistoryService;
 
     @PostMapping(value = "employees/{empId}/previous-work-histories",headers="version=v1")
-    public List<PreviousWorkHistoryResponse> createPreviousWorkHistoty(@PathVariable Long empId, @RequestBody List<PreviousWorkHistoryRequest> previousWorkHistoryRequests)throws EmployeeNotFoundException{
-        return previousWorkHistoryService.createPreviousWorkHistoryDetails(empId,previousWorkHistoryRequests);
+    public List<PreviousWorkHistoryResponse01> createPreviousWorkHistory(@PathVariable Long empId, @RequestBody List<PreviousWorkHistoryDTO> previousWorkHistoryDTOS)throws EmployeeNotFoundException{
+        return previousWorkHistoryService.createPreviousWorkHistoryDetails(empId,previousWorkHistoryDTOS);
     }
 
     @GetMapping(value = "employees/{eid}/previousWorkHistories",headers="version=v1")
-    public List<PreviousWorkHistoryResponse> getPreviousWorkHistoryByEmpId(@PathVariable("eid") Long empId) throws EmployeeNotFoundException {
+    public List<PreviousWorkHistoryResponse01> getPreviousWorkHistoryByEmpId(@PathVariable("eid") Long empId) throws EmployeeNotFoundException {
         return previousWorkHistoryService.getPreviousWorkHistoryByEmpId(empId);
     }
 
-    @PutMapping(value= "employees/{empId}/previousWorkHistories/{historyId}",headers="version=v1")
-    public PreviousWorkHistoryIdResponse updatePreviousWorkHistoryDetails(@PathVariable Long empId, @PathVariable Long historyId,
-                                                                          @RequestBody PreviousWorkHistoryRequest updatedHistoryRequest) throws EmployeeNotFoundException, PreviousWorkHistoryNotFoundException {
-        return previousWorkHistoryService.updatePreviousWorkHistoryDetails(empId,historyId,updatedHistoryRequest);
-    }
+//    @PutMapping(value= "employees/{empId}/previousWorkHistories/{historyId}",headers="version=v1")
+//    public PreviousWorkHistoryIdResponse updatePreviousWorkHistoryDetails(@PathVariable Long empId, @PathVariable Long historyId,
+//                                                                          @RequestBody PreviousWorkHistoryRequest updatedHistoryRequest) throws EmployeeNotFoundException, PreviousWorkHistoryNotFoundException {
+//        return previousWorkHistoryService.updatePreviousWorkHistoryDetails(empId,historyId,updatedHistoryRequest);
+//    }
 
     @DeleteMapping(value = "employees/{empId}/previousWorkHistories/{historyId}",headers = "version=v1")
     public String deleteEmergencyContactById(@PathVariable("empId") Long empId, @PathVariable("historyId") Long previousWorkHistoryId) throws EmployeeNotFoundException,PreviousWorkHistoryNotFoundException {
